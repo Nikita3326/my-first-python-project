e3a2c3a68176e0d08a8630c1159283f9f7da3e2d
Nikita3326 <nikita3025114@gmail.com>
2025-06-24 20:03:51 +0530

    Create hypotenuse.py
    program that asks the user for two numbers, a and b, and then calculates the hypotenuse c

---
+++ hypotenuse.py
@@ -0,0 +1,7 @@
+#program that asks the user for two numbers, a and b, and then calculates the hypotenuse c
+a=int(input("Enter the shortest side:"))
+b=int(input("Enter another one:"))
+c=(a**2)+(b**2)
+h=c**(1/2)
+print(h)
+
